c5b926703a59835c887ca39c8afdb1b87ebefb8d
Honey the Bear <53951776+HoneyTheYellowBear123@users.noreply.github.com>
2026-01-23 14:51:04 -0500

    first pass at main page

---
+++ src/frontpage.ts
@@ -0,0 +1,13 @@
+//header
+//picture of me
+//educational and technical summary and what I'm looking for
+import React, { Component } from 'react';
+
+function nameHeader(){
+    return (
+        <div>
+        <h1>Mitchell Mazzoni</h1>
+        <h2>Software Engineer</h2>
+        </div>
+    )
+}
