e0e7ed3e05dc4ca6369f8499f4a430b1e02c5903
Honey the Bear <53951776+HoneyTheYellowBear123@users.noreply.github.com>
2026-02-11 16:34:02 -0500

    formatting for navbar on professional portfolio fixed

--- newbase/src/pages/TheProZone/TheProZoneMain.tsx
+++ newbase/src/pages/TheProZone/TheProZoneMain.tsx
@@ -8,8 +8,9 @@ import NavBar from "../../components/navBar"
 function NameHeader(){
     return (
         <>
-            <div>
-                <h1>Professional Portfolio</h1>
+            <div className="ProfessionalSummary">
+                <h1>Mitchell Mazzoni - Software Engineer</h1>
+                <h2>Hello! I am a well rounded software engineer with a combined four years of professional experience across devops, computer vision, machine learning, and simulation. I completed my Master's in Computer Science with a specialization in machine learning at the Georgia Institute of Technology in 2025 and am currently pursuing new job opportunities. I develop and modify video games as a hobby, which has involved significant work in C#, SQL, and open source contributions, and the software I developed with this hobby has a collective 140K+ downloads. Most recently I have been pursuing AWS certifications and developing this website, after familiarizing myself with typescript, react, and node.</h2>
             </div>
         </>
     )
