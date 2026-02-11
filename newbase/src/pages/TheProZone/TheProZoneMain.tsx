1ebd443e696d530a353e52a2583f19e030f2c792
Honey the Bear <53951776+HoneyTheYellowBear123@users.noreply.github.com>
2026-02-11 16:01:01 -0500

    got the list elements aligned the way I want

--- newbase/src/pages/TheProZone/TheProZoneMain.tsx
+++ newbase/src/pages/TheProZone/TheProZoneMain.tsx
@@ -1,6 +1,6 @@
 //navigate between engineering qualifications, and educational projects
 
-
+import './TheProZoneMain.css'
 //navigates between Music, The workout zone, and the "does mitchell have a crush on you page", video game modding
 //TO DO: make name header a bunch of random colors for each letter
 import NavBar from "../../components/navBar"
@@ -15,11 +15,26 @@ function NameHeader(){
     )
 }
 
+function Education(){
+    return (
+        <>
+            <div>
+                <h3>Education</h3>
+                <ul>
+                    <li>Georgia Institute of Technology - BS in Aerospace Engineering - 2019</li>
+                    <li>Georgia Institute of Technology - MS in Computer Science with Machine Learning Specialization - 2025</li>
+                </ul>
+            </div>
+        </>
+    )
+}
+
 function TheProZoneMain(){
     return (
         <div>
             <NavBar />
             <NameHeader />
+            <Education />
         </div>
     )
 }
